4982576dd506d1a78bab9e0ed71a4a1feabf5117
Tim Cameron Ryan <id@timryan.org>
2015-02-12 12:53:15 -0800

    Revert "Merge pull request #38 from tessel/tcr-573"
    This reverts commit 597b2b4419a841b1e6cfc53ca540699086a89a6e, reversing
    changes made to 6ff929b1e8d7fce66c9af9b0f01c384f012f9ba6.
    
    Conflicts:
    	package.json

--- src/colonize.js
+++ src/colonize.js
@@ -565,8 +565,6 @@ node.finalizer ? bodyjoin(node.finalizer.body) : '',
   } else if (type == 'FunctionExpression' || type == 'FunctionDeclaration') {
     var localstr = colony_locals[0].length ? 'local ' + colony_locals[0].join(', ') + ' = ' + colony_locals[0].join(', ') + ';\n' : '';
     var usesArguments = !!colony_locals[0].arguments;
-    var strictMode = false;
-    var callee = node.id ? hygenifystr(node.id) : usesArguments ? '_callee' : null;
     var hoistsr = colony_locals[0].hoist.join('\n');
     colony_locals.shift()
     colony_flow = colony_locals[0].flow;
@@ -575,16 +573,16 @@ node.finalizer ? bodyjoin(node.finalizer.body) : '',
     }
     var fnnode = colony_node(node,
       (type == 'FunctionDeclaration' ? (node.id ? hygenifystr(node.id) + ' = ' : '') + '' : '(')
-      + (callee ? '(function () local ' + hygenifystr(callee) + ' = nil; ' + hygenifystr(callee) + ' = ' : '')
+      + (node.id ? '(function () local ' + hygenifystr(node.id) + ' = nil; ' + hygenifystr(node.id) + ' = ' : '')
       + 'function ('
       + (usesArguments
-        ? 'this, ...)\n' + (node.params.length ? 'local ' + node.params.map(hygenifystr).join(', ') + ' = ...;\n' : '') + 'local arguments = _arguments(' + JSON.stringify(strictMode) + ', ' + (callee || 'nil') + ', ...);\n'
+        ? 'this, ...)\n' + (node.params.length ? 'local ' + node.params.map(hygenifystr).join(', ') + ' = ...;\n' : '') + 'local arguments = _arguments(...);\n'
         : ['this'].concat(node.params.map(hygenifystr)).join(', ') + ')\n')
       + localstr
       + hoistsr
       + bodyjoin(node.body.body)
       + '\nend'
-      + (callee ? '; ' + hygenifystr(callee) + ':__defineGetter__("name", function () return ' + JSON.stringify(hygenifystr(callee)) + '; end); return ' + hygenifystr(callee) + '; end)()' : '')
+      + (node.id ? '; ' + hygenifystr(node.id) + ':__defineGetter__("name", function () return ' + JSON.stringify(hygenifystr(node.id)) + '; end); return ' + hygenifystr(node.id) + '; end)()' : '')
       + (type == 'FunctionDeclaration' ? ';\n' : ')'));
 
     if (type == 'FunctionDeclaration') {
